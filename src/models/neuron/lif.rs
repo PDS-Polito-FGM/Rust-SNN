0b084bb98ed01e1e6b7900620dcb0c84b7abee98
FRANCESCO ROSATI s296247 <s296247@studenti.polito.it>
2022-10-19 18:07:51 +0200

    Added initial neuron configuration

--- src/models/neuron/lif.rs
+++ src/models/neuron/lif.rs
@@ -1,10 +1,30 @@
+use crate::snn::neuron::Neuron;
+
 // * LIF submodule *
 
 /**
     Object representing a Neuron in the LIF (Leaky Integrate-and-Fire) model
 */
-pub struct LifNeuron {  // test
-    pub l: f64
+pub struct LifNeuron {
+    v_th: f64,
+    v_rest: f64,
+    v_reset: f64,
+    tau: f64,
+    v_mem_ts_prev: f64,
+    ts_prev: u64
+}
+
+impl LifNeuron {
+    pub fn new(v_th: f64, v_rest: f64, v_reset: f64, tau: f64, v_mem_ts_prev: f64, ts_prev: u64) -> Self {
+        Self { v_th, v_rest, v_reset, tau, v_mem_ts_prev, ts_prev }
+    }
+}
+
+impl Neuron for LifNeuron {
+    fn compute_v_mem(t: u64, weighted_sum: f64) -> f64 {
+        //Implement the function with the right formula
+        0.0
+    }
 }
 
 // TODO: implement LIF Neuron struct - implement Neuron Trait
--- src/snn/neuron.rs
+++ src/snn/neuron.rs
@@ -5,5 +5,5 @@
     It represents a general Neuron of a Layer
 */
 pub trait Neuron {
-    // TODO: implement Neuron trait
+    fn compute_v_mem(t: u64, weighted_sum: f64) -> f64;
 }

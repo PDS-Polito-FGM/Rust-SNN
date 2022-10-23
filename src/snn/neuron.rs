189518f49aefa7ffd38d9eb013ce5ec837a4dae8
Mario Mastrandrea s302219 <mario.mastrandrea@asp-poli.it>
2022-10-23 18:46:14 -0500

    Changes by Pino

--- src/snn/neuron.rs
+++ src/snn/neuron.rs
@@ -5,5 +5,5 @@
     It represents a general Neuron of a Layer
 */
 pub trait Neuron {
-    fn compute_v_mem(&mut self,t: u64, weighted_sum: f64) -> u8;
+    fn compute_v_mem(&mut self, t: u64, extra_weighted_sum: f64, intra_weighted_sum: f64) -> u8;
 }

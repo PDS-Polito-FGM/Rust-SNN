355df69a68c67d73b77c4b2f6a2f7e5b71f27cc4
giuseppelazzara98 <s302064@studenti.polito.it>
2022-11-18 15:30:44 +0100

    Name changed Mario :)

--- src/snn/builders/mod.rs
+++ src/snn/builders/mod.rs
@@ -1,7 +1,7 @@
 // * submodules *
 
 pub use self::builder::SnnBuilder;
-pub use self::alternative_builder::DynSnnBuilder;
+pub use self::dyn_builder::DynSnnBuilder;
 // types of builder
 mod builder;
-mod alternative_builder;
+mod dyn_builder;

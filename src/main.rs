864ae39e7121fe56d8dfc6318641b845c388db89
FRANCESCO ROSATI s296247 <s296247@studenti.polito.it>
2022-12-12 22:46:50 +0100

    Fixed stack problem

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,30 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 use std::time::Instant;
 use colored::Colorize;
-use pds_snn::builders::{SnnBuilder, DynSnnBuilder};
+use pds_snn::builders::DynSnnBuilder;
 use pds_snn::models::neuron::lif::LifNeuron;
 
 fn main() {
 
-    const N_NEURONS: usize = 400;
-    const N_INPUTS: usize = 784;
-    const N_INSTANTS: usize = 3500;
+    let n_neurons: usize = 400;
+    let n_inputs: usize = 784;
+    let n_instants: usize = 3500;
 
-    let _input_spikes: [[u8; N_INSTANTS]; N_INPUTS] = read_input_spikes();
+    let input_spikes: Vec<Vec<u8>> = read_input_spikes(n_instants, n_inputs);
 
-    let neurons: [LifNeuron; N_NEURONS] = build_neurons();
+    let neurons: Vec<LifNeuron> = build_neurons(n_neurons);
 
-    let extra_weights: [[f64; N_NEURONS]; N_NEURONS] = read_extra_weights();
+    let extra_weights: Vec<Vec<f64>> = read_extra_weights(n_neurons, n_inputs);
 
-    let intra_weights: [[f64; N_NEURONS]; N_NEURONS] = build_intra_weights();
+    let intra_weights: Vec<Vec<f64>> = build_intra_weights(n_neurons);
 
     println!("{}","Building the SNN network...".yellow());
 
     let building_start = Instant::now();
 
-    /* SNN network */
-    /*
-    let _snn = SnnBuilder::new()
-        .add_layer()
-        .weights(extra_weights)
-        .neurons(neurons)
-        .intra_weights(intra_weights)
-        .build();
-     */
-
     /*  Dynamic SNN network  */
-    let _snn = DynSnnBuilder::new(N_INPUTS)
-        .add_layer(neurons.to_vec(),
-                   extra_weights.map(|el| el.to_vec()).to_vec(),
-                   intra_weights.map(|el| el.to_vec()).to_vec())
+    let mut snn = DynSnnBuilder::new(n_inputs)
+        .add_layer(neurons, extra_weights, intra_weights)
         .build();
 
     let building_end = building_start.elapsed();
@@ -50,7 +38,8 @@ fn main() {
 
     let computing_start = Instant::now();
 
-    //TODO: Call the process function to compute the real output spikes - Francesco
+    /* calling the dynSNN process function */
+    snn.process(&input_spikes);
 
     let computing_end = computing_start.elapsed();
 
@@ -58,8 +47,9 @@ fn main() {
 
     println!("{}", format!("\nTime elapsed in computing the output spikes: {}.{:03} seconds\n", computing_end.as_secs(), computing_end.subsec_millis()).blue());
 
-    let _output_spikes: [[u8; N_INSTANTS]; N_NEURONS] = [[0; N_INSTANTS]; N_NEURONS];
-    write_to_output_file(_output_spikes);
+    let output_spikes: Vec<Vec<u8>> = vec![vec![0; n_instants]; n_neurons];
+
+    write_to_output_file(output_spikes, n_neurons, n_instants);
 
 }
 
@@ -68,39 +58,39 @@ fn main() {
 /**
     This function builds the neurons of the network.
 */
-fn build_neurons<const N_NEURONS: usize>() -> [LifNeuron; N_NEURONS] {
+fn build_neurons(n_neurons: usize) -> Vec<LifNeuron> {
 
-    let thresholds: [f64; N_NEURONS] = read_thresholds();
+    let thresholds: Vec<f64> = read_thresholds(n_neurons);
 
     let v_rest: f64 = -65.0;
     let v_reset: f64 = -60.0;
     let tau: f64 = 100.0;
 
-    let mut neurons: Vec<LifNeuron> = Vec::with_capacity(N_NEURONS);
+    let mut neurons: Vec<LifNeuron> = Vec::with_capacity(n_neurons);
 
-    for i in 0..N_NEURONS {
+    for i in 0..n_neurons {
 
         let neuron = LifNeuron::new(thresholds[i], v_rest, v_reset, tau);
 
         neurons.push(neuron);
     }
 
-    neurons.try_into().unwrap()
+    neurons
 }
 
 /**
-    This function builds a 2D array of intra weights
+    This function builds a 2D Vec of intra weights
 */
-fn build_intra_weights<const N_NEURONS: usize>() -> [[f64; N_NEURONS]; N_NEURONS] {
+fn build_intra_weights(n_neurons: usize) -> Vec<Vec<f64>> {
 
     let value: f64 = -15.0;
 
-    let mut intra_weights: [[f64; N_NEURONS]; N_NEURONS] = [[0f64; N_NEURONS]; N_NEURONS];
+    let mut intra_weights: Vec<Vec<f64>> = vec![vec![0f64; n_neurons]; n_neurons];
 
     println!("{}","Computing intra weights...".yellow());
 
-    for i in 0..N_NEURONS {
-        for j in 0..N_NEURONS {
+    for i in 0..n_neurons {
+        for j in 0..n_neurons {
             if i == j {
                 intra_weights[i][j] = 0.0;
             } else {
@@ -115,14 +105,14 @@ fn build_intra_weights<const N_NEURONS: usize>() -> [[f64; N_NEURONS]; N_NEURONS
 }
 
 /**
-    This function reads the input spikes from the input file and returns a 2D array of u8.
+    This function reads the input spikes from the input file and returns a 2D Vec of u8.
 */
-fn read_input_spikes<const N_INSTANTS: usize, const N_INPUTS: usize>() -> [[u8; N_INSTANTS]; N_INPUTS] {
+fn read_input_spikes(n_instants: usize, n_inputs: usize) -> Vec<Vec<u8>> {
     let path_input = "inputSpikes.txt";
     let input = File::open(path_input).expect("Something went wrong opening the file inputSpikes.txt!");
     let buffered = BufReader::new(input);
 
-    let mut input_spikes: [[u8; N_INSTANTS]; N_INPUTS] = [[0; N_INSTANTS]; N_INPUTS];
+    let mut input_spikes: Vec<Vec<u8>> = vec![vec![0; n_instants]; n_inputs];
 
     println!("{}","Reading input spikes from file inputSpikes.txt...".yellow());
 
@@ -144,14 +134,14 @@ fn read_input_spikes<const N_INSTANTS: usize, const N_INPUTS: usize>() -> [[u8; 
 }
 
 /**
-    This function reads the weights file and returns a 2D array of weights
+    This function reads the weights file and returns a 2D Vec of weights
 */
-fn read_extra_weights<const N_NEURONS: usize, const N_INPUTS: usize>() -> [[f64; N_INPUTS]; N_NEURONS] {
+fn read_extra_weights(n_neurons: usize, n_inputs: usize) -> Vec<Vec<f64>> {
     let path_weights_file = "./networkParameters/weightsOut.txt";
     let input = File::open(path_weights_file).expect("Something went wrong opening the file weightsOut.txt!");
     let buffered = BufReader::new(input);
 
-    let mut extra_weights: [[f64; N_INPUTS]; N_NEURONS] = [[0f64; N_INPUTS]; N_NEURONS];
+    let mut extra_weights: Vec<Vec<f64>> = vec![vec![0f64; n_inputs]; n_neurons];
 
     println!("{}","Reading weights from file weightsOut.txt...".yellow());
 
@@ -160,7 +150,7 @@ fn read_extra_weights<const N_NEURONS: usize, const N_INPUTS: usize>() -> [[f64;
     for line in buffered.lines() {
         let split: Vec<String> = line.unwrap().as_str().split(" ").map(|el| el.to_string()).collect::<Vec<String>>();
 
-        for j in 0..N_INPUTS {
+        for j in 0..n_inputs {
             extra_weights[i][j] = split[j].parse::<f64>().expect("Cannot parse string into f64!");
         }
 
@@ -173,14 +163,14 @@ fn read_extra_weights<const N_NEURONS: usize, const N_INPUTS: usize>() -> [[f64;
 }
 
 /**
-    This function reads the threshold file and returns an array of thresholds
+    This function reads the threshold file and returns a Vec of thresholds
 */
-fn read_thresholds<const N_NEURONS: usize>() -> [f64; N_NEURONS] {
+fn read_thresholds(n_neurons: usize) -> Vec<f64> {
     let path_threshold_file = "./networkParameters/thresholdsOut.txt";
     let input = File::open(path_threshold_file).expect("Something went wrong opening the file thresholdsOut.txt!");
     let buffered = BufReader::new(input);
 
-    let mut thresholds: [f64; N_NEURONS] = [0f64; N_NEURONS];
+    let mut thresholds: Vec<f64> = vec![0f64; n_neurons];
 
     println!("{}","Reading thresholds from file thresholdsOut.txt...".yellow());
 
@@ -201,16 +191,16 @@ fn read_thresholds<const N_NEURONS: usize>() -> [f64; N_NEURONS] {
 /**
     This function writes the output spikes to the output file.
 */
-fn write_to_output_file<const N_NEURONS: usize, const N_INSTANTS: usize>(output_spikes: [[u8; N_INSTANTS]; N_NEURONS]) -> () {
+fn write_to_output_file(output_spikes: Vec<Vec<u8>>, n_neurons: usize, n_instants: usize) -> () {
     let path_output = "outputCounters.txt";
     let mut output_file = File::create(path_output).expect("Something went wrong opening the file outputCounters.txt!");
 
-    let mut neurons_sum: [u32; N_NEURONS] = [0; N_NEURONS];
+    let mut neurons_sum: Vec<u32> = vec![0; n_neurons];
 
     println!("{}","Computing sum of the spikes for each neuron...".yellow());
 
-    for i in 0..N_NEURONS {
-        for j in 0..N_INSTANTS {
+    for i in 0..n_neurons {
+        for j in 0..n_instants {
             neurons_sum[i] += output_spikes[i][j] as u32;
         }
     }
@@ -219,7 +209,7 @@ fn write_to_output_file<const N_NEURONS: usize, const N_INSTANTS: usize>(output_
 
     println!("{}","Writing data into file outputCounters.txt...".yellow());
 
-    for i in 0..N_NEURONS {
+    for i in 0..n_neurons {
         output_file.write_all(format!("{}\n", neurons_sum[i]).as_bytes()).expect("Something went wrong writing into the file outputCounters.txt!");
     }
 
